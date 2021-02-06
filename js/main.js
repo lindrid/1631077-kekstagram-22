7c71e8eace988b60049d27ba5b178b2a83585cbf
Lindrid <lindrid32@gmail.com>
2021-02-06 14:57:33 +1000

    Добавил файл main.js в index.html, сделал 2 задачу кексограма

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const isWrongRange = (min,max) => (min > max || min < 0 || max < 0);
+
+/**
+ * https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random#%D0%BF%D0%BE%D0%BB%D1%83%D1%87%D0%B5%D0%BD%D0%B8%D0%B5_%D1%81%D0%BB%D1%83%D1%87%D0%B0%D0%B9%D0%BD%D0%BE%D0%B3%D0%BE_%D1%86%D0%B5%D0%BB%D0%BE%D0%B3%D0%BE_%D1%87%D0%B8%D1%81%D0%BB%D0%B0_%D0%B2_%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%BD%D0%BE%D0%BC_%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%B2%D0%B0%D0%BB%D0%B5_%D0%B2%D0%BA%D0%BB%D1%8E%D1%87%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D0%BE 
+ */
+const getRandomInt = function (min, max) {
+  if (isWrongRange(min, max)) {
+    return NaN;
+  }
+  if (min === max) {
+    return max;
+  }
+
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
+}
+
+const stringHasMaxLength = (str, maxLength) => str.length <= maxLength;
+
+getRandomInt(1, 10);
+stringHasMaxLength('aaa', 2);
