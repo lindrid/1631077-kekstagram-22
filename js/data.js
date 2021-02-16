22e4791aa4c8a655a1ecefa2761f557ae1440bd4
Lindrid <lindrid32@gmail.com>
2021-02-17 07:22:33 +1000

    1)index.html: type=module 2) создал модули data и util, вынес туда функционал

---
+++ js/data.js
@@ -0,0 +1,85 @@
+import {getRandomInt, stringHasMaxLength} from './util.js';
+
+const COMMENT_MAX_LENGTH = 140;
+
+const generateObjects = function (quantity) {
+  const MAX_COMMENTS_NUMBER = 20;
+
+  let id = 1;
+  let commentsIds = [];
+
+  return new Array(quantity).fill(0).map(() => {
+    const url = `photos/${id}.jpg`;
+    const description = `Фотография #${id}`;
+    const commentsNumber = getRandomInt(1, MAX_COMMENTS_NUMBER);
+    const likes = getRandomInt(15, 200);
+    
+    const messages = [
+      'Всё отлично!',
+      'В целом всё неплохо. Но не всё.',
+      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. ' +
+        'В конце концов это просто непрофессионально.',
+      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота ' +
+        'и у меня получилась фотография лучше.',
+      'Лица у людей на фотке перекошены, как будто их избивают. Как можно ' + 
+        'было поймать такой неудачный момент?!',
+    ];
+    const names = [
+      'Артем',
+      'Роман',
+      'Аркадий',
+      'Александр',
+      'Василий',
+      'Алиса',
+      'Екатерина',
+      'Полина',
+      'Ирина',
+      'Людмила',
+    ];
+
+    const canMessageStayWithName = function (messageIndex, nameIndex) {
+      return !(messageIndex === 4 && nameIndex > 4);
+    }
+
+    const comments = new Array(commentsNumber).fill(0).map(() => {
+      let id;
+      do {
+        id = getRandomInt(1, quantity * MAX_COMMENTS_NUMBER);
+      } while (commentsIds.includes(id));
+      commentsIds.push(id);
+
+      const avatar = `img/avatar-${getRandomInt(1, 6)}.svg`;
+      
+      let message, i;
+      do {
+        i = getRandomInt(0, messages.length - 1);
+      } while (!stringHasMaxLength(messages[i], COMMENT_MAX_LENGTH));
+      message = messages[i];
+
+      let j;
+      do {
+        j = getRandomInt(0, names.length - 1);
+      }
+      while (!canMessageStayWithName(i, j))
+      const name = names[j];
+
+      return {
+        id: id,
+        avatar: avatar,
+        message: message,
+        name: name,
+      };
+    });
+
+    return {
+      id: id++,
+      url: url,
+      description: description,
+      likes: likes,
+      comments: comments,
+    };
+  });
+}
+
+export {generateObjects};
