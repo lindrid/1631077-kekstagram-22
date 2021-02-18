4e40ddc2c1002ad73efe2cf1fabadef2279e7465
Lindrid <lindrid32@gmail.com>
2021-02-18 11:55:04 +1000

    рефакторинг

--- js/data.js
+++ js/data.js
@@ -4,6 +4,30 @@ const COMMENT_MAX_LENGTH = 140;
 const MAX_COMMENTS_NUMBER = 20;
 
 const generateObjects = function (quantity) {
+  const messages = [
+    'Всё отлично!',
+    'В целом всё неплохо. Но не всё.',
+    'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. ' +
+      'В конце концов это просто непрофессионально.',
+    'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+    'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота ' +
+      'и у меня получилась фотография лучше.',
+    'Лица у людей на фотке перекошены, как будто их избивают. Как можно ' + 
+      'было поймать такой неудачный момент?!',
+  ];
+  const names = [
+    'Артем',
+    'Роман',
+    'Аркадий',
+    'Александр',
+    'Василий',
+    'Алиса',
+    'Екатерина',
+    'Полина',
+    'Ирина',
+    'Людмила',
+  ];
+
   const generateUniqueId = function (ids, maxRandom) {
     let id;
     do {
@@ -13,6 +37,10 @@ const generateObjects = function (quantity) {
     return id;
   }
 
+  const canMessageStayWithName = function (messageIndex, nameIndex) {
+    return !(messageIndex === 4 && nameIndex > 4);
+  }
+
   let objectsIds = [];
   let commentsIds = [];
 
@@ -20,38 +48,10 @@ const generateObjects = function (quantity) {
     const id = generateUniqueId(objectsIds, 25);
     const url = `photos/${id}.jpg`;
     const description = `Фотография #${id}`;
-    const commentsNumber = getRandomInt(1, MAX_COMMENTS_NUMBER);
+    const commentsLength = getRandomInt(1, MAX_COMMENTS_NUMBER);
     const likes = getRandomInt(15, 200);
-    
-    const messages = [
-      'Всё отлично!',
-      'В целом всё неплохо. Но не всё.',
-      'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. ' +
-        'В конце концов это просто непрофессионально.',
-      'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
-      'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота ' +
-        'и у меня получилась фотография лучше.',
-      'Лица у людей на фотке перекошены, как будто их избивают. Как можно ' + 
-        'было поймать такой неудачный момент?!',
-    ];
-    const names = [
-      'Артем',
-      'Роман',
-      'Аркадий',
-      'Александр',
-      'Василий',
-      'Алиса',
-      'Екатерина',
-      'Полина',
-      'Ирина',
-      'Людмила',
-    ];
 
-    const canMessageStayWithName = function (messageIndex, nameIndex) {
-      return !(messageIndex === 4 && nameIndex > 4);
-    }
-
-    const comments = new Array(commentsNumber).fill(0).map(() => {
+    const comments = new Array(commentsLength).fill(0).map(() => {
       let id = generateUniqueId(commentsIds, quantity * MAX_COMMENTS_NUMBER);
       const avatar = `img/avatar-${getRandomInt(1, 6)}.svg`;
       
